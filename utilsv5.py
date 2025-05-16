f6a543c20325b8123fc50affa35255b412f4135d
SpangeWenkies <jessegeerts1@gmail.com>
2025-05-16 18:10:56 +0200

    Clean up of file structure & names

--- utilsv5.py
+++ utilsv5.py
@@ -1,9 +1,41 @@
 import numpy as np
 from scipy.stats import norm
 from scipy.stats import laplace
+import sys
 
+def WhiteNoiseSim(iT, vDistrParams, sDistrName, sPlot):
+    """
+    Purpose:
+        Generate white noise
 
+    Inputs:
+        iT              sample size
+        vDistrParams    distributional params
+        sDistrName      distribution name
+        bPlot           boolean for plotting series, default is 1
 
+    Return value:
+        vEps            white noise realisations
+
+    Output:
+        Plot realisations process
+
+    """
+    lDistrName = ['normal', 't']
+    if (sDistrName in lDistrName):
+        if (sDistrName == lDistrName[0]):
+            vEps = np.random.randn(iT)
+        elif (sDistrName == lDistrName[1]):
+            vEps = np.random.standard_t(vDistrParams[0], size=iT)
+
+        # if (sPlot == 'line'):
+        #     Plot(vEps, "White noise", True)
+        # elif (sPlot == 'hist'):
+        #     Plot(vEps, "White noise", False)
+        return vEps
+    else:
+        sys.exit(
+            "Distribution not supported.")  # print("Error: Distribution not supported. Supported distributions are:"); print(*lDistrName, sep = ", ");
 def ScoreIndicator(list_of_nums, single_y, left_tail, q):
     if q == 0:
         indicator = np.array([1 if single_y < np.percentile(list_of_nums, left_tail) else 0])
